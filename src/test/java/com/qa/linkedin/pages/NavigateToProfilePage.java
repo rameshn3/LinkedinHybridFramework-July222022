9fb7050b1f09563180f6d57ddbf12af8bab42bc5
Ramesh Ch <rameshn3@gmail.com>
2022-08-01 21:05:37 -0500

    added code for naviagteToProgilePage class

--- src/test/java/com/qa/linkedin/pages/NavigateToProfilePage.java
+++ src/test/java/com/qa/linkedin/pages/NavigateToProfilePage.java
@@ -1,6 +1,14 @@
-package 
+package com.qa.linkedin.pages;
   
-  public class NavigateToProfilePage{
-    
+  public class NavigateToProfilePage extends BasePageWebActions{
+
+	private Logger log=Logger.getLogger(NavigateToProfilePage.class);
+	
+	public NavigateToProfilePage() {
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(xpath = "//a[@class='app-aware-link'][contains(.,'See all people results')]")
+	private WebElement seeAllPeopleResultsLink;
     
   }
